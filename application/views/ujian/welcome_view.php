e77899329717b2b641118bb185ac6c314a74c312
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-02 22:54:16 +0700

    left

--- application/views/ujian/welcome_view.php
+++ application/views/ujian/welcome_view.php
@@ -14,19 +14,12 @@
 
 	
 </script>
-<script>
-window.ChatraSetup = {
-    colors: {
-        buttonText: '#f0f0f0', /* chat button text color */
-        buttonBg: '#043687'    /* chat button background color */
-    }
-};
-</script>
+
 <script>
 window.ChatraSetup = {
     buttonPosition: window.innerWidth < 1024? // width threshold
         'bl': // chat button position on small screens
-        'br'  // chat button position on big screens
+       
 };
 </script>
 <!--/Chatra {/literal} -->
